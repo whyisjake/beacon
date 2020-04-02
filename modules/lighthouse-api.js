5f87b9edc61af3941a8385abdbcc70606ad99783
Jake Spurlock <whyisjake@gmail.com>
2020-04-02 10:10:11 -0700

    Clean up some eslint issues.

--- modules/lighthouse-api.js
+++ modules/lighthouse-api.js
@@ -7,17 +7,15 @@ const client = new Redis();
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
   return chromeLauncher.launch({ chromeFlags: opts.chromeFlags }).then((chrome) => {
-    opts.port = chrome.port;
+    opts.port = chrome.port; // eslint-disable-line no-param-reassign
     return lighthouse(url, opts, config).then((results) =>
       // use results.lhr for the JS-consumable output
       // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
       // use results.report for the HTML/JSON/CSV output as a string
       // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
       // The resizer script is allows some comms between the iframe window so it can be resized.
-      chrome.kill().then(() => {
-        client.set(url, results.report + resizer.script, () => {
-          return results.lhr;
-        });
+      chrome.kill().then(() => { // eslint-disable-line implicit-arrow-linebreak
+        client.set(url, results.report + resizer.script, () => results.lhr);
       }));
   });
 }
@@ -25,7 +23,7 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
 function getResults(url, opts) {
   return client.get(url, (err, data) => {
     if (err || data === null) {
-      console.log( 'We need to get the data for ' + url);
+      console.log(`We need to get the data for ${url}`); // eslint-disable-line no-console
       return launchChromeAndRunLighthouse(url, opts);
     }
     return data;
